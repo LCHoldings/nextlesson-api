7eb2a3d52f6adc578dbc2bb94d8b07ebbff3a34f
Lazyllama <me@lazyllama.xyz>
2024-11-11 19:58:13 +0100

    No dist.

--- dist/jest.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/test'],
-};
-exports.default = config;
